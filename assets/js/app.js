0ef2efa43cc53c09f88dab3bdc6e1a95e8778ac8
j8sh <j8shigarcia@aol.com>
2018-04-24 10:09:58 -0500

    updating github so i can go remote

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,12 @@ $('.menu-toggle').click(function() {
     $('.site-nav').toggleClass('site-nav--open');
     $(this).toggleClass('open');
 })
+
+// This is for the to task webpage
+ function todoList(){
+     var item = document.getElementById("todoInput").value;
+     var text = document.createTextNode(item);
+     var newItem = document.createElement("li");
+     newItem.appendChild(text);
+     document.getElementById("todoList").appendChild(newItem);
+ }
