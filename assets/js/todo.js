105736d612c00e53978ec88a90126b5af0d599df
j8sh <j8shigarcia@aol.com>
2018-04-28 17:05:11 -0500

    added to todo.js file

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,11 +1,38 @@
-// first collect data from the input form
+const form = document.getElementById('textInput');
+const input = form.querySelector('input');
+const ul = document.getElementById('taskList');
 
-function textInput () {
-var task = document.getElementById('inputTodo').value;
-};
+function createLI(text) {
+const li = document.createElement('li');
+  li.textContent = text;
+  const label = document.createElement('label');
+  label.textContent = 'Confirmed';
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  label.appendChild(checkbox);
+  li.appendChild(label);
+  const button = document.createElement('button');
+  button.textContent = 'remove';
+  li.appendChild(button);
+  return li;
+}
 
-// secondly, adding the data to an array
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const text = input.value;
+  input.value = '';
+  const li = createLI(text);
+  ul.appendChild(li);
+});
 
-var todoList = [];
-todoList.push(textInput());
-document.getElementById("taskList").innerHTML = todoList;
+ul.addEventListener('change', (e) => {
+  const checkbox = event.target;
+  const checked = checkbox.checked;
+  const listItem = checkbox.parentNode.parentNode;
+  
+  if (checked) {
+    listItem.className = 'responded';
+  } else {
+    listItem.className = '';
+  }
+});
